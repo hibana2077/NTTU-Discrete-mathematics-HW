3e9c693c1130530ad03351f3b5eb5516c00205ec
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-12-11 15:58:29 +0800

    Update RSA_final.py

--- source/RSA_final.py
+++ source/RSA_final.py
@@ -22,23 +22,40 @@ def write_json_file(file_name, data):
         json.dump(data, f)
     return True
 
+def d_and_n(p,q,e):
+    d = pow(e,-1,(p-1)*(q-1))
+    n = p * q
+    return d,n
+
+def encrypt(data,e,n):
+    data += ' ' if len(data)%2 else ''
+    pre_encrypt_data = "".join([char_table[i] for i in data])
+    encrypt_data = [int(pre_encrypt_data[i:i+4]) for i in range(0,len(pre_encrypt_data),4)]
+    encrypt_data = [pow(i,e,n) for i in encrypt_data]
+    return encrypt_data
+
+def decrypt(data,d,n):
+    return [pow(i,d,n) for i in data]
+
 #input data
 if input("input data from file? (y/n): ") == 'y':
     temp = read_json_file("data.json")
     p,q,e,data = temp['p'],temp['q'],temp['e'],temp['data']
+    print(f"p: {p}")
+    print(f"q: {q}")
+    print(f"e: {e}")
+    print(f"data: {data}")
 else:
     p,q,e,data= int(input("input p: ")),int(input("input q: ")),int(input("input e: ")),input("input data: ")
 out = "OUTPUT"
 print(f"{out:=^50}")
-d,n = pow(e,-1,(p-1)*(q-1)),p * q
+d,n = d_and_n(p,q,e)
+encrypt_data = encrypt(data,e,n)
+decrypt_data = decrypt(encrypt_data,d,n)
+
 print(f"d: {d}")
 print(f"n: {n}")
-data += ' ' if len(data)%2 else ''
-pre_encrypt_data = "".join([char_table[i] for i in data])
-encrypt_data = [int(pre_encrypt_data[i:i+4]) for i in range(0,len(pre_encrypt_data),4)]
-encrypt_data = [pow(i,e,n) for i in encrypt_data]
 print(f"encrypt data(C): {' '.join(list(map(str,encrypt_data)))}")
-decrypt_data = [pow(i,d,n) for i in encrypt_data]
 print(f"decrypt data(M): {' '.join(list(map(str,decrypt_data)))}")
 print(f"{out:=^50}")
 if input("save data to file? (y/n): ") == 'y':
